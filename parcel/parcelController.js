889304f8baa23fd9d418582659f2206afbf0c1d2
SachinNegi10 <sachinnegi2002@gmail.com>
2025-07-01 12:40:15 +0530

    Zero_miles_delivery

--- parcel/parcelController.js
+++ parcel/parcelController.js
@@ -5,7 +5,7 @@ const { insertParcel, getAllParcels, getParcelById } = require('./parcelService'
 router.post('/', (req, res) => {
     insertParcel(req.body, (err) => {
         if (err) return res.status(500).send(err.message);
-        // console.log("Parcel Created:", req.body);
+        console.log("Parcel Created:", req.body);
         res.status(201).send('Parcel created');
     });
 });
@@ -21,6 +21,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     getParcelById(req.params.id, (err, row) => {
         if (err) return res.status(500).send(err.message);
+        console.log(req.params.id);
         if (!row) return res.status(404).send('Parcel not found');
         res.status(200).json(row);
     });
